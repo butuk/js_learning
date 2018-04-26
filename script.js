d66602881120fec401e2f897a3df1c4fe66f9f9b
Vitali Yanusheuski <vitaliyanusheuski@Vitalis-MacBook-Pro.local>
2018-04-26 15:48:46 +0300

    Задание, в котором всё должно работать, но не работает

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-var answer = prompt ('Каково «официальное» название JavaScript?', '');
-if (answer = 'ECMAScript'){
-	alert('Верно!');
+var who = prompt ('Кто пришёл?', '');
+
+if (who == 'Админ') {
+	var password = prompt ('Введите пароль', '');
+
+	if (password == 'Чёрный Властелин') { 
+		alert ('Добро пожаловать!');
+	
+	} else if (password == null) || (password == '') {
+	alert ('Вход отменён');
+	
+	} else {
+		alert ('Пароль неверен');
+	}
+
+} else if (who == null) || (password == '') {
+	alert ('Вход отменён');
+
 } else {
-	alert('Не знаете?');
-}
+	alert ('Я вас не знаю');
